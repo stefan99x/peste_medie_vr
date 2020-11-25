94a1d8bc9363414a26814c6f86347d59257d0992
Stefan Guliciuc <stefan.guliciuc@fabritglobal.com>
2020-11-25 14:35:21 +0200

    <2020-11-25><Initial commit>

---
+++ Assets/HeadMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMovement : MonoBehaviour
+{
+    public Transform Head;
+    public float HeadSpeed;     // ( ͡° ͜ʖ ͡°) 
+    float HeadAngle;
+
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //source.Play();
+        HeadAngle += Input.GetAxis("Mouse X") * HeadSpeed * -Time.deltaTime;
+        HeadAngle = Mathf.Clamp(HeadAngle, 0, 30f);
+        Head.localRotation = Quaternion.AngleAxis(HeadAngle, Vector3.up);   // <= rotate only on the Y axis
+    }
+}
---
+++ Assets/MovementLArm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementLArm : MonoBehaviour
+{
+    public Transform LeftArm;
+    public float LeftArmSpeed;
+    public float LeftArmAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LeftArmAngle += 0.2f * LeftArmSpeed * -Time.deltaTime;
+        LeftArmAngle = Mathf.Clamp(LeftArmAngle, 0, 45f);
+        LeftArm.localRotation = Quaternion.AngleAxis(LeftArmAngle, Vector3.left);   // <= rotate only on the Y axis
+    }
+}
