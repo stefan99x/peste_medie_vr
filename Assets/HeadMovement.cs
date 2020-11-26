f66d5c5e2a08385afb62c8d9edff3801d97e7749
Stefan Guliciuc <stefan.guliciuc@fabritglobal.com>
2020-11-26 14:35:32 +0200

    <2020-11-26><Added the changes>

--- Assets/HeadMovement.cs
+++ Assets/HeadMovement.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
-    public Transform Head;
-    public float HeadSpeed;     // ( ͡° ͜ʖ ͡°) 
-    float HeadAngle;
-
-    public AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
-        //source.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
         //source.Play();
-        HeadAngle += Input.GetAxis("Mouse X") * HeadSpeed * -Time.deltaTime;
-        HeadAngle = Mathf.Clamp(HeadAngle, 0, 30f);
-        Head.localRotation = Quaternion.AngleAxis(HeadAngle, Vector3.up);   // <= rotate only on the Y axis
+        var screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.LookAt(screenPoint);
     }
 }
